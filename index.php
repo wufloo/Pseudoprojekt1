6c3010b015c2ebb13bca2e3a712d225d3ac30f7e
Janre <janre.press@khk.ee>
2015-11-04 13:05:42 +0200

    24. Require refactor
    [Delivers #103975370]

--- index.php
+++ index.php
@@ -9,15 +9,20 @@
 </body>
 </html>
 
-
+<?php
+$page = (!empty($_GET['page']) ? $_GET['page'] : 'home');
+?>
 <form action="?">
-    <input name="page">
-    <input type="submit">
-</form>
+    <form action="index.php?" method="get">
+        <input name="page">
+        <input type="submit">
+    </form>
 
 <?php
 
 if (isset($_GET['page'])) {
     require $_GET['page'] . '.php';
 }
+
+require($page . '.php');
 ?>
