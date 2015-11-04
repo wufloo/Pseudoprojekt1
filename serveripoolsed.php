5b15448ea2d327cf53f65c6ba338951ab4770a81
Janre <janre.press@khk.ee>
2015-11-04 12:55:01 +0200

    16. Massiivid
    [Delivers #103975350]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -6,12 +6,25 @@ $perekonnanimi = "Press";
 $vanus = "21";
 echo "$eesnimi $perekonnanimi ($vanus)";
 
+$janre = new isik;
+
 $vowels = array('A', 'E', 'I', 'O', 'U', 'Õ', 'Ä', 'Ö', 'Ü', 'a', 'e', 'i', 'o', 'u', 'õ', 'ä', 'ö', 'ü');
 if (in_array(substr($janre->eesnimi, 0, 1), $vowels)) {
     $eesnimi_algab_vokaaliga = true;
 } else {
     $eesnimi_algab_vokaaliga = false;
 }
+echo "<br>";
+class isik
+{
+    var $eesnimi = "Janre";
+    var $perekonnanimi = "Press";
+    var $vanus = 21;
+
+}
+
+$janre = new isik;
+echo "" . $janre->eesnimi . " " . $janre->perekonnanimi . " (" . $janre->vanus . ") ";
 
 
 
