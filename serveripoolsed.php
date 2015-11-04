e15e4141ea7a6131a1ba5b049774e787c3172057
Janre <janre.press@khk.ee>
2015-11-04 12:52:05 +0200

    15. Boolean
    [Delivers #103975348]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -5,4 +5,15 @@ $eesnimi = "Janre";
 $perekonnanimi = "Press";
 $vanus = "21";
 echo "$eesnimi $perekonnanimi ($vanus)";
+
+$vowels = array('A', 'E', 'I', 'O', 'U', 'Õ', 'Ä', 'Ö', 'Ü', 'a', 'e', 'i', 'o', 'u', 'õ', 'ä', 'ö', 'ü');
+if (in_array(substr($janre->eesnimi, 0, 1), $vowels)) {
+    $eesnimi_algab_vokaaliga = true;
+} else {
+    $eesnimi_algab_vokaaliga = false;
+}
+
+
+
+
 ?>
