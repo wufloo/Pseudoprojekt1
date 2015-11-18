a357c0538a13294d32ea2dedd991f68c8f062fb4
Janre <janre.press@khk.ee>
2015-11-18 09:32:13 +0200

    19. Funktsioonid PARANDUS
    [Delivers #103975358]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,17 +1,25 @@
 <?php
-$firstname = "Janre";
-$lastname = "Press";
-$age = 21;
+class isik
+{
+    var $firstname = "Janre";
+    var $lastname = "Press";
+    var $age = 21;
+    var $sex = "<br /> Mees";
+}
 
-$person = array(
-    "firstname" => "Janre",
-    "lastname" => "Press",
-    "age" => "21"
-);
+$janre = new isik;
 
-$eesnimi_algab_vokaaliga = false;
+$vowels = array('A', 'E', 'I', 'O', 'U', 'Õ', 'Ä', 'Ö', 'Ü', 'a', 'e', 'i', 'o', 'u', 'õ', 'ä', 'ö', 'ü');
+if (in_array(substr($janre->firstname, 0, 1), $vowels)) {
+    $eesnimi_algab_vokaaliga = true;
+} else {
+    $eesnimi_algab_vokaaliga = false;
+}
 
-echo $firstname . " " . $lastname . " ($age)";
-echo "<p>" . $person['firstname'] . " " . $person['lastname'] . " (" . $person['age'] . ")</p>";
+if ($eesnimi_algab_vokaaliga) {
+    echo "<p>Nimi algab vokaaliga</p>";
+};
+
+echo "<p>" . $janre->firstname . " " . $janre->lastname . " (" . $janre->age . ") " . $janre->sex . "</p>";
 
 ?>
